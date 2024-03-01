b226b22c4f9801a4da353ac3454d3a1de82c0913
Aslan <aslambek.hudajev@gmail.com>
2024-03-01 16:29:39 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { QrScanner } from "@yudiel/react-qr-scanner";
 function App() {
   return (
     <div className="App">
+      <h1>Skanna QR kod</h1>
       <QrScanner
         onResult={(result) => (window.location.href = result)}
         onDecode={(result) => console.log(result)}
